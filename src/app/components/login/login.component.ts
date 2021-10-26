fd789f02d56bb4303cac3f916d87cfe5fa891662
ErickCM14 <erickcarranzameza@gmail.com>
2021-10-26 11:36:24 -0500

    2

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup = this.fb.group({
+    correo: [, [Validators.required]],
+    contrasena: [, [Validators.required, Validators.pattern(/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/)]]
+  });
+  cargando: boolean = true
+
+  constructor(private _auth: AuthService, private fb: FormBuilder) {
+    
+   }
 
   ngOnInit(): void {
+    // this.inicializar()
+  }
+
+  inicializar(){
+    this.loginForm = this.fb.group({
+      correo: [, [Validators.required]],
+      contrasena: [, [Validators.required, Validators.pattern(/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/)]]
+    });
+    this.cargando = false;
+  }
+
+  guardar(){
+
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,53 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { AuthModel } from '../models/auth.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
-
   private key: string = `AIzaSyAkdG1ayXiDaoqHirWwaU50-44VV_R7QcA`;
   private realDatabase: string = 'https://tarjetadigitalda-default-rtdb.firebaseio.com';
+
+  private apiAuth = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + this.key;
+
+  public userToken: any;
+
+  constructor( private http: HttpClient ) { }
+
+  Login(usuario: AuthModel) {
+
+    const authData = {
+      email: usuario.correo,
+      password: usuario.contrasena,
+      returnSecureToken: true
+    };
+
+    return this.http.post(
+      `${this.apiAuth}`,
+      authData
+    ).pipe(map(resp => {
+      // console.log('Entro al mapa RXJS');
+      this.guardarToken(resp);
+      // this.esAdmin();
+      return resp;
+    })
+    );
+  }
+
+  public guardarToken(idToken: any) {
+
+    this.userToken = idToken['idToken'];
+    let id = idToken['localId']
+
+    sessionStorage.setItem('token', idToken['idToken']);
+    sessionStorage.setItem('refresh_token', idToken['refreshToken']);
+    sessionStorage.setItem('email', idToken['email']);
+    sessionStorage.setItem('name', idToken['displayName']);
+    sessionStorage.setItem('idUsuario', idToken['localId']);
+  }
+
 
 }
