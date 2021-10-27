0ca85d3c0deca5eac96f312ef205de67f1755c00
Omar Jimenez <omar_solanoxnr@hotmail.com>
2021-10-27 17:07:21 -0500

    Agregando componente de registro y clientes, tambien se agrego el bootraps y la tipografia. se creo tambien la vista del registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
 import { VisitaComponent } from './components/visita/visita.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'visita', component: VisitaComponent},
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'registro', component: RegistroComponent},
   {path: '**' , redirectTo: 'login'}
 ];
 
