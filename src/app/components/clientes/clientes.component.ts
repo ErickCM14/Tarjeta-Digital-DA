8f24e0dfb55a8b73e2cf5e1639da6d93373fbdb2
Omar Jimenez <omar_solanoxnr@hotmail.com>
2021-10-29 10:07:57 -0500

    Agregando vista de clientes

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-clientes',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
-
-  constructor() { }
+  getClientes: any[] = [];
+  constructor( private AuthService:AuthService) { }
 
   ngOnInit(): void {
+    this.AuthService.getCliente().subscribe(resp => {
+      this.getClientes = resp;
+      console.log(this.getClientes);
+    })
   }
 
 }
